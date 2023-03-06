a7f934cb44fadd146efc3a749ac5aa2f17af57f3
Keegan <keegancolaco20@gmail.com>
2023-03-06 23:00:13 +0530

    Popup Message added'

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//dark-mode
 let icon = document.getElementById('icon');
 icon.onclick = function () {
     document.body.classList.toggle("dark-theme");
@@ -11,3 +12,34 @@ icon.onclick = function () {
         icon.src ="img/moon.png";
     }
 }
+
+//customise popup on clicking submit
+const popupContainer = document.getElementById('container');
+const popupMessage = document.getElementById('popup-message');
+const contactForm = document.getElementById('contact-form');
+const headerContact =document.getElementById('hcontact');
+
+contactForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const formData = new FormData(contactForm);
+  const response = await fetch('https://formspree.io/f/mwkjbjkr', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json'
+    },
+    body: formData
+  });
+  if (response.ok) {
+    contactForm.reset();
+    popupContainer.style.display = 'none';
+    headerContact.style.display='none';
+    popupMessage.style.display = 'block';
+    setTimeout(() => {
+      popupMessage.style.display = 'none';
+      location.href = location.href;
+    },80000);
+
+  } else {
+    alert('Error submitting form!');
+  }
+});
